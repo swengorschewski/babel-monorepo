34a5874b7a06d262d33e1aaba8a9b13ba8ef6cb2
Swen Gorschewski <swen.gorschewski@gmail.com>
2020-08-14 09:59:22 +0200

    initial commit

---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyComponent from "../../components/my-component";
+
+export default App = () => {
+  return (
+    <>
+      <div>App</div>
+      <MyComponent />
+    </>
+  );
+};
